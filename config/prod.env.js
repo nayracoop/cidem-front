394bdaaec3ff7d18add1e82c9371fb3321c994e9
martucol <martucolmenero@gmail.com>
2018-11-26 12:41:13 -0300

    change server_path

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-//  SERVER_PATH: '"http://staging.nayra.coop/cidem/public/api"'
-  SERVER_PATH: '"https://192.168.0.165/public/api"'
+  SERVER_PATH: '"http://staging.nayra.coop/cidem/public/api"'
+//  SERVER_PATH: '"https://192.168.0.165/public/api"',
 }
