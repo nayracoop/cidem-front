54f71b975d5732325823012999b476c90c37a3c3
Pehuén <pehuen.rodriguez@nayra.coop>
2018-12-20 12:08:12 -0300

    Bug fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_PATH: '"https://staging.nayra.coop/cidem/public/api"'
+  SERVER_PATH: '"https://staging.nayra.coop/cidem/public/api"',
   BASE_URL: '"/cidemuntref/"'
 }
